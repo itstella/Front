df908850ff03b3fa5232f1313d0a8c74e3520ac8
Stella <90361308+sngooo@users.noreply.github.com>
2022-08-22 01:26:53 -0500

    cards

--- src/components/headers/homeHeader.js
+++ src/components/headers/homeHeader.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import tw from 'twin.macro';
-import Profile from '../images/bonsai.png';
+import Profile from '../images/corgi.jpeg';
 
 const Container = tw.div`py-8 
-bg-[#f4f4f4] hover:bg-dustyPink transition duration-700 ease-in-out
-flex items-center justify-around 
+bg-[#f4f4f4] hover:bg-dustyPink transition duration-300 ease-in-out
+ items-center justify-around 
 
 `;
-const ProfilePicture = tw(Profile) `rounded-full w-10 h-10`;
+const ProfilePicture = tw(Profile) `rounded-full w-1 h-1`;
 const Header = tw.h1`text-hotPink text-center text-6xl`;
 const Subheader = tw.h4``;
 
@@ -15,9 +15,9 @@ const Subheader = tw.h4``;
 function App() {
   return (
     <Container>
-        {/* <Profile /> */}
       <Header>Hello World</Header>
-
+      {/* <Profile />  */}
+      <Header>Images</Header>
     </Container>
   );
 }
