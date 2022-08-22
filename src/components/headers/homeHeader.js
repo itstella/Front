2b33281933f69bd1eedc7beb667d79996b8d7441
Stella <90361308+sngooo@users.noreply.github.com>
2022-08-22 02:41:56 -0500

    nothing

--- src/components/headers/homeHeader.js
+++ src/components/headers/homeHeader.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import tw from 'twin.macro';
-import Corgi from '../images/corgi.jpeg';
+import Shrimp from '../images/shrimp.jpeg';
 import Cards from '../features/cards.js';
 
 const Container = tw.div`py-8 
@@ -8,7 +8,8 @@ bg-[#f4f4f4] hover:bg-dustyPink transition duration-300 ease-in-out
  items-center justify-around `
  ;
 
- const ImageContainer = tw.div`flex justify-center items-center`;
+ const ImageContainer = tw.div`flex justify-center items-center w-1/3 h-1/3 mx-auto mb-8`;
+ 
 
 const Header = tw.h1`text-hotPink text-center text-6xl`;
 const Subheader = tw.h4``;
@@ -19,7 +20,7 @@ function App() {
     <Container>
       <Header>Hello World</Header>
       <ImageContainer>
-      <img src={Corgi}  alt={''} />
+      <img src={Shrimp}  alt={''} />
       </ImageContainer>
       
       <Cards />
