7c41f62f9d3736a536d8632fbbe8e28bc9f0c95f
Emmanuel Mendieta <emmanuel_mendieta@icloud.com>
2022-08-22 01:04:01 -0500

    Header

--- src/components/headers/homeHeader.js
+++ src/components/headers/homeHeader.js
@@ -2,7 +2,11 @@ import React from 'react'
 import tw from 'twin.macro';
 import Profile from '../images/bonsai.png';
 
-const Container = tw.div`py-8`;
+const Container = tw.div`py-8 
+bg-[#f4f4f4] hover:bg-dustyPink transition duration-700 ease-in-out
+flex items-center justify-around 
+
+`;
 const ProfilePicture = tw(Profile) `rounded-full w-10 h-10`;
 const Header = tw.h1`text-hotPink text-center text-6xl`;
 const Subheader = tw.h4``;
@@ -13,6 +17,7 @@ function App() {
     <Container>
         {/* <Profile /> */}
       <Header>Hello World</Header>
+
     </Container>
   );
 }
